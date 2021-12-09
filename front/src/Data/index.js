487433b89b6facc0bc147beb13587bce1fbaedd1
Hong <tjdqls1668@ajou.ac.kr>
2021-12-09 22:32:38 +0900

    Update data

--- front/src/Data/index.js
+++ front/src/Data/index.js
@@ -1,16 +1,27 @@
-import IT1 from './it/2021-10-31';
-import IT2 from './it/2021-11-07';
-import IT3 from './it/2021-11-14';
-import IT4 from './it/2021-11-21';
-import politics1 from './politics/2021-11-16';
-import politics2 from './politics/2021-11-24';
+import IT1 from './it/2021-10-31-14';
+import IT2 from './it/2021-11-07-20';
+import IT3 from './it/2021-11-14-18';
+import IT4 from './it/2021-11-21-19';
+import IT5 from './it/2021-11-29-24';
+import IT6 from './it/2021-12-05-21';
+import politics1 from './politics/2021-11-16-20';
+import politics2 from './politics/2021-11-24-23';
+import politics2 from './politics/2021-11-30-19';
+import politics2 from './politics/2021-12-07-22';
 
 export default {
   IT: {
     '10월_4주차': IT1,
     '11월_1주차': IT2,
     '11월_2주차': IT3,
-    '11월_3주차': IT4
+    '11월_3주차': IT4,
+    '11월_4주차': IT5,
+    '12월_1주차': IT6
   },
-  politics: { '11월_2주차': politics1, '11월_3주차': politics2 }
+  politics: {
+    '11월_2주차': politics1,
+    '11월_3주차': politics2,
+    '11월_4주차': politics3,
+    '12월_1주차': politics4
+  }
 };
