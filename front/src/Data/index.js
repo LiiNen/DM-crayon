9066c380fddf214211e869df0c449ea05e1c1e6a
Hong <tjdqls1668@ajou.ac.kr>
2021-12-09 23:03:11 +0900

    Update data

--- front/src/Data/index.js
+++ front/src/Data/index.js
@@ -4,7 +4,6 @@ import IT3 from './it/2021-11-14-18';
 import IT4 from './it/2021-11-21-19';
 import IT5 from './it/2021-11-29-24';
 import IT6 from './it/2021-12-05-21';
-import IT7 from './it/2021-11';
 import politics1 from './politics/2021-11-16-20';
 import politics2 from './politics/2021-11-24-23';
 import politics3 from './politics/2021-11-30-19';
@@ -17,7 +16,6 @@ export default {
     '11월_2주차': IT3,
     '11월_3주차': IT4,
     '11월_4주차': IT5,
-    '11월_종합': IT7,
     '12월_1주차': IT6
   },
   politics: {
